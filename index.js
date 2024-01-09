b37c55ae9826f1dca1232db04d83ad34472e618c
Andres Yanquen <andres.yanquen@hotmail.com>
2024-01-09 09:24:43 -0500

    Change color after validation

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const newVariable1 = 'test';
 const newVariable2 = 'test';
 const newVariable3 = 'test';
 const newVariable4 = 'test';
+
+
+setTimeout(() => {
+    desc.style.color = "red";
+    desc.style.backgroundColor = "green"; 
+}, 2000);
