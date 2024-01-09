7f94238be6b307a8255f7f647d06263c71753aa8
Andres Yanquen <andres.yanquen@hotmail.com>
2024-01-09 09:23:32 -0500

    new feature completed
    more variables
    
    Change color after validation

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ desc.style.color = "green";
 desc.style.backgroundColor = "red"; 
 
 const newVariable = 'test';
+const newVariable1 = 'test';
+const newVariable2 = 'test';
+const newVariable3 = 'test';
+const newVariable4 = 'test';
+
+
+setTimeout(() => {
+    desc.style.color = "red";
+    desc.style.backgroundColor = "green"; 
+}, 2000);
